73c8f46fae5d07601127b8e7d597d37fa3283010
pejma2001 <pejma2001@mail.ru>
2020-07-01 18:20:51 +0400

    New commit

---
+++ 3.3-async/task.js
@@ -0,0 +1,14 @@
+class alarmClock {
+	constructor{
+		constructor(){
+		alarmCollection() = [];
+		timerId(id);
+	}
+	addClock(){
+		setTimeout(){
+			let date = new Date;
+			const start = date.getHours() + "." + date.getMinutes();
+		}
+	}
+	}
+}
