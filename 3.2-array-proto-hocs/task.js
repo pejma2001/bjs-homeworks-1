58ebf6973968801775e07d046fea4a430232c8d5
pejma2001 <pejma2001@mail.ru>
2020-07-03 08:37:25 +0400

    Commit new

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -11,17 +11,37 @@ function sum(...args) {
     return sum += +arg;
   }, 0);
 }
-function compareArrays( arr1, arr2 ){
-	if(arr1.every[] == arr2.every[]){
-     return true;
-	}else{
+function compareArrays( arr1, arr2 ) {
+	if(arr1.length == arr2.length) {
 		return false;
-	}
+	}else {
+        arr1.every(
+        	function compare(arr1[],index) {
+               if(arr1[] == arr2[] || arr1.index == arr2.index) {
+               	  return true;
+               }else {
+               	  return false;
+
+               }
+        	})
+}
+
+
+
+	
 }
 function memorize(fn, limit){
+	const innerFunc = () => "Hello";
+	return innerFunc();
+	
+
+}
+const result = memorize();
+result(1,2,3,4);
+/*	
 	const mSum = memorize(sum, 5); 
 sum(3, 4); 
 
 mSum(3, 4); 
 const memory = [mSum];
-}
+}*/
