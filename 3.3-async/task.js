05332f832fe3a37aab1cc3ee54fb4c0fe3dfe66d
pejma2001 <pejma2001@mail.ru>
2020-07-04 17:00:28 +0400

    New commit

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,14 +1,35 @@
 class alarmClock {
-	constructor{
-		constructor(){
-		alarmCollection() = [];
-		timerId(id);
+
+	constructor(alarmCollection, timerId){
+		this.alarmCollection = [];
+		this.timerId;
 	}
-	addClock(){
-		setTimeout(){
-			let date = new Date;
-			const start = date.getHours() + "." + date.getMinutes();
+	addClock(time, callback, id){
+		if(id == undefine){
+			throw new Error ("Внимание! Ошибка!");
+		}else if(this.alarmCollection.find(id)){
+			console.error("Ошибка! Такой id уже существует");
+		}else{
+			const collect = new alarmCol(id, time, callback);
+			this.alarmCollection.push(collect);
 		}
+    
+    }
+    removeClock(id){
+    	let current = this.alarmCollection.filter(id);
+    	this.alarmCollection.splice(current,1);
+    }
+
+    getCurrentFormattedTime(){
+    	let now = new Date();
+    	let time = now.getHours() + now.getMinutes();
+    	return time;
+    }
+
+    start(){
+    	function checkClock(ring){
+          if()
+    	}
+    }
 	}
-	}
-}
+	
